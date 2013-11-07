fa8c93c0ea0d3903bf48dd7602ffa74978151737
github <le@mail.keleyi.com>
2013-11-07 22:53:43 +0800

    0.1.4
    Keleyi 0.1.4.Browser Support:IE6+,Chrome,Firefox,Opera,Safari

--- jquery.keleyi.js
+++ jquery.keleyi.js
@@ -1,6 +1,6 @@
 ﻿/*!
 * Keleyi(jQuery Menu)
-* version: 0.1.3
+* version: 0.1.4
 * Copyright (c) 2013 KeLeyi
 * http://keleyi.com
 * http://keleyi.com/keleyi/
@@ -26,19 +26,44 @@
         $(this).css({ "width": settings.width, "margin": settings.margin });
 
         $(this).wrap("<div class='keleyi-menubar'></div>");
-        $(this).parent().css({ "background-color": settings.bar_background_color
+        var k_bar = $(this).parent();
+        k_bar.css({ "background-color": settings.bar_background_color
         , "height": settings.bar_height, "position": settings.bar_position
         , "bottom": settings.bar_bottom, "min-width": settings.width
         });
 
-        $(this).parent().append("<div style='width:100%;clear:both;height:0px;'></div>");
+        if (! -[1, ] && !window.XMLHttpRequest) {
+            if (k_bar.css("position") == "fixed") {
+                ie6FixedBottom(k_bar, settings.bar_bottom);
+                $(this).find("a").css("text-decoration", "none").hover(function () { $(this).css("text-decoration", "underline") }
+                , function () { $(this).css("text-decoration", "none") });
+            }
+        }
 
-        $(this).find(">li").css({ "width": settings.item_width, "background-color": settings.item_background_color, "margin": settings.item_margin });
+        $(this).parent().append("<div style='width:100%;clear:both;height:0px;overflow:hidden'></div>");
+
+        $(this).find(">li").css({ "width": settings.item_width, "background-color": settings.item_background_color, "margin": settings.item_margin
+        , "padding": "0px", "white-space": "nowrap", "height": "20px", "float": "left", "text-align": "center"
+        , "display": "inline-block", "position": "relative"
+        });
+
+        $(this).find(">li a").css({ "color": "white", "line-height": "20px"
+        , "display": "block", "font-size": "14px", "width": "100%"
+        });
+
+        $(this).find(">li ul").css({ "padding": "0px", "list-style-type": "none"
+        , "background-color": "transparent", "position": "absolute", "display": "none"
+        });
 
         $(this).find(">li>a").mouseover(function () {
             $(this).parent().css({ "background-color": settings.item_background_color_hover });
             var k_ul = $(this).parent().find("ul");
-            k_ul.css({ "background-color": settings.item_background_color_hover, "top": $(this).parent().position().top - (k_ul.height()) }).show();
+            if (k_ul.length < 1)
+                return;
+
+            k_ul.css({ "background-color": settings.item_background_color_hover, "top": $(this).parent().position().top - (k_ul.height())
+                , "left": "0px", "margin": "0px"
+            }).show();
             if (k_ul.width() < $(this).parent().width())
                 k_ul.width($(this).parent().width());
         });
@@ -47,5 +72,19 @@
             $(this).find("ul").hide();
             $(this).css("background-color", settings.item_background_color);
         });
+
+        function ie6FixedBottom(fixedobj, bottommargin) {
+            fixedobj.css({ "position": "absolute" });
+            var k_bm = new Number;
+            k_bm = Number(bottommargin.substring(0, bottommargin.length - 2));
+            var obj = fixedobj[0];
+            function setStyleTop() {
+                obj.style.top = document.documentElement.scrollTop + document.documentElement.clientHeight
+                - obj.offsetHeight - (parseInt(obj.currentStyle.marginTop, 0) || k_bm) - (parseInt(obj.currentStyle.marginBottom, 0) || k_bm)
+            }
+            window.onscroll = function () { setStyleTop(); }
+            window.onresize = function () { setStyleTop(); }
+        }
+
     }
 } (jQuery));
