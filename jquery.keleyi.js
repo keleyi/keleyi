3f8ea0bef8fc7f32fa15215c07b6b505174c1ccc
keleyi <le@mail.keleyi.com>
2013-11-24 23:45:21 +0800

    0.1.6
    0.1.6 link color

--- jquery.keleyi.js
+++ jquery.keleyi.js
@@ -1,6 +1,6 @@
 ﻿/*!
 * Keleyi(jQuery Menu)
-* version: 0.1.5
+* version: 0.1.6
 * Copyright (c) 2013 KeLeyi
 * http://keleyi.com
 * http://keleyi.com/keleyi/
@@ -11,7 +11,7 @@
         var settings = $.extend({
             width: '986px',
             margin: '0px auto',
-            isAutoAddTriangle:true,
+            isAutoAddTriangle: true,
             item_background_color_hover: '#005500',
             item_background_color: 'transparent',
             item_width: 'auto',
@@ -19,13 +19,13 @@
             bar_height: 'auto',
             bar_position: 'fixed',
             bar_background_color: "#008000",
-            bar_bottom: "0px"
+            bar_bottom: "0px",
+            mainItem_color: "white",
+            subItem_color:"white"
         }, options);
-
 
         $(this).addClass("keleyi-menu");
         $(this).css({ "width": settings.width, "margin": settings.margin });
-
 
         $(this).wrap("<div class='keleyi-menubar'></div>");
         var k_bar = $(this).parent();
@@ -33,7 +33,6 @@
         , "height": settings.bar_height, "position": settings.bar_position
         , "bottom": settings.bar_bottom, "min-width": settings.width
         });
-
 
         if (! -[1, ] && !window.XMLHttpRequest) {
             if (k_bar.css("position") == "fixed") {
@@ -43,25 +42,27 @@
 
         $(this).parent().append("<div style='width:100%;clear:both;height:0px;overflow:hidden'></div>");
 
-
         $(this).find(">li").css({ "width": settings.item_width, "background-color": settings.item_background_color, "margin": settings.item_margin
         , "padding": "0px", "white-space": "nowrap", "height": "20px", "float": "left", "text-align": "center"
         , "display": "inline-block", "position": "relative"
         });
 
-
         $(this).find(">li a").css({ "color": "white", "line-height": "20px"
         , "display": "block", "font-size": "14px"
-        });
+        });        
 
 
-        $(this).find(">li").find("a:first").css({ "width": "100%", "overflow": "hidden" });
         if (settings.isAutoAddTriangle)
-        $(this).find(">li").has('ul').find("a:first").append("<b></b>");
+            $(this).find(">li").has('ul').find("a:first").append("<b></b>");
+
+        $(this).find(">li").find("a:first").css({ "width": "100%", "overflow": "hidden", "color": settings.mainItem_color });
+        $(this).find(">li").find("b").css({ "border-color": ("transparent transparent " + settings.mainItem_color) })
 
         $(this).find(">li ul").css({ "padding": "0px", "list-style-type": "none"
         , "background-color": "transparent", "position": "absolute", "display": "none"
         });
+
+        $(this).find(">li").find("li a").css({ "color": settings.subItem_color });
 
         $(this).find(">li>a").mouseover(function () {
             $(this).parent().css({ "background-color": settings.item_background_color_hover });
